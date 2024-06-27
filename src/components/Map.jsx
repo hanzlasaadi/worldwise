3819507a9ef5f01acb2dcde1d6d6d40b01b0c608
HanzlaLatitude <hanzalasaadi@gmail.com>
2024-06-27 20:03:35 +0500

    chore: map with leaflet

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,19 +1,32 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import styles from "./Map.module.css";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 
 function Map() {
   const [searchParams, setSearchParams] = useSearchParams();
   const nav = useNavigate();
 
+  const lat = searchParams.get("lat") || 51.505;
+  const lng = searchParams.get("lng") || -0.09;
+
   return (
     <div className={styles.mapContainer} onClick={() => nav("form")}>
-      <h1>Map</h1>
-      <h1>
-        Position: {searchParams.get("lat")} {searchParams.get("lng")}
-      </h1>
-      <button onClick={() => setSearchParams({ lat: 50, lng: 80 })}>
-        Click me
-      </button>
+      <MapContainer
+        className={styles.map}
+        center={[lat, lng]}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        <Marker position={[lat, lng]}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
     </div>
   );
 }
